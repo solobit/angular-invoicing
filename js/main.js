d436b61317eee6426e0c726f6a18fee873160057
supersym <maildumpert@gmail.com>
2014-03-15 03:23:55 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+function formattedDate(date) {
+    var d = new Date(date || Date.now()),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [day, month, year].join('/');
+}
 
 function InvoiceController($scope) {
 
@@ -127,7 +138,7 @@ function readURL(input) {
 
 $(document).ready(function() {
   
-  $("#invoice_date").val(Date.now());
+  $("#invoice_date").val(formattedDate());
   $("#invoice_number").focus();
   $("#imgInp").change(function() {
     readURL(this);
